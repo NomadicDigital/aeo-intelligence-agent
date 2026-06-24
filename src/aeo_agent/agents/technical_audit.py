b0d2d7a86b1c30cd840e1d019424e16050cc5193
Matt Hall <matthall8@gmail.com>
2026-06-24 12:10:13 +0100

    feat(technical-audit): add llms.txt and llms-full.txt checks with timeout handling and added trailing slash strip and request time to robots checker

--- src/aeo_agent/agents/technical_audit.py
+++ src/aeo_agent/agents/technical_audit.py
@@ -3,6 +3,7 @@ from typing import Dict
 import requests
 
 AI_CRAWLERS = ["GPTBot", "ClaudeBot", "Google-Extended", "PerplexityBot", "OAI-SearchBot"]
+TIMEOUT_TIME = 10
 
 def find_robot_rule(agent, robots_txt_contents) -> str:
     """
@@ -30,11 +31,21 @@ def check_robots_txt(input_url) -> Dict:
     Function for checking the website's Robots.txt file for initial existance. If it exists, 
     we store the raw contents and then check for any explicit rules for LLMBots.
     """
-    robots_url = f"{input_url}/robots.txt"
+    clean_url = input_url.rstrip('/')
+    robots_url = f"{clean_url}/robots.txt"
     blocked_crawlers, allowed_crawlers, unmentioned_crawlers = [],[],[]
     
     # Step 1: Fetch robots.txt
-    r = requests.get(robots_url)
+    try:
+        r = requests.get(robots_url, timeout=TIMEOUT_TIME)
+    except requests.exceptions.Timeout:
+        error_msg = f"Robots timed out"
+        return {
+            "robots_txt": {
+                "exists": False
+            },
+            "errors": [error_msg]
+        }
 
     # Step 2: Handle failure cases
     if r.status_code == 404:
@@ -55,34 +66,81 @@ def check_robots_txt(input_url) -> Dict:
         }
 
     # Step 3: If exists, check each AI crawler
-    if r.status_code == 200:
-        robots_txt_contents = r.text
-        for crawler in AI_CRAWLERS:
-            if f"User-agent: {crawler}" in robots_txt_contents:
-                rule = find_robot_rule(crawler, robots_txt_contents)
+    robots_txt_contents = r.text
+    for crawler in AI_CRAWLERS:
+        if f"User-agent: {crawler}" in robots_txt_contents:
+            rule = find_robot_rule(crawler, robots_txt_contents)
+            if rule == 'Allow':
+                allowed_crawlers.append(crawler)
+            elif rule == 'Disallow':
+                blocked_crawlers.append(crawler)
+            else:
+                unmentioned_crawlers.append(crawler)
+        elif "User-agent: *" in robots_txt_contents:
+                rule = find_robot_rule("*", robots_txt_contents)
                 if rule == 'Allow':
                     allowed_crawlers.append(crawler)
                 elif rule == 'Disallow':
                     blocked_crawlers.append(crawler)
-                else:
-                    unmentioned_crawlers.append(crawler)
-            elif "User-agent: *" in robots_txt_contents:
-                    rule = find_robot_rule("*", robots_txt_contents)
-                    if rule == 'Allow':
-                        allowed_crawlers.append(crawler)
-                    elif rule == 'Disallow':
-                        blocked_crawlers.append(crawler)
-            else:
-                unmentioned_crawlers.append(crawler)
+        else:
+            unmentioned_crawlers.append(crawler)
+    return {
+        "robots_txt": {
+            "exists": True,
+            "raw_content": robots_txt_contents,
+            "blocked_ai_crawlers": blocked_crawlers,
+            "allowed_ai_crawlers": allowed_crawlers,
+            "unmentioned_ai_crawlers": unmentioned_crawlers
+        }
+    }
+    
+def check_llm_files(input_url, file_name, state_key):
+    """
+    Function for checking the website's LLMs.txt and LLMS-full file for initial existance. If it exists, 
+    we store the raw contents.
+    """
+    clean_url = input_url.rstrip('/')
+    llm_file_url = f"{clean_url}/{file_name}"
+    # Step 1: Fetch file
+    try:
+        r = requests.get(llm_file_url, timeout=TIMEOUT_TIME)
+    except requests.exceptions.Timeout:
+        error_msg = f"{file_name} timed out"
         return {
-            "robots_txt": {
-                "exists": True,
-                "raw_content": robots_txt_contents,
-                "blocked_ai_crawlers": blocked_crawlers,
-                "allowed_ai_crawlers": allowed_crawlers,
-                "unmentioned_ai_crawlers": unmentioned_crawlers
+            f"{state_key}": {
+                "exists": False
+            },
+            "errors": [error_msg]
+        }
+    
+    # Step 2: Handle failure cases
+    if r.status_code == 404:
+        return {
+            f"{state_key}": {
+                "exists": False
+            }
+        }
+    
+    elif r.status_code != 200:
+        error_msg = f"{file_name} response failed for with status code: {r.status_code}"
+        print(f"❌ {error_msg}")
+        return {
+            f"{state_key}": {
+                "exists": False
+            },
+            "errors": [error_msg]
+        }
+    
+    file_contents = r.text
+    return {
+        f"{state_key}":
+        {
+            "exists": True,
+            "raw_content": file_contents
             }
         }
 
 def technical_audit(state:AgentState) -> AgentState:
-    return {}
+    extracted_robots_txt = check_robots_txt(state['input_url'])
+    extracted_llms_txt = check_llm_files(state['input_url'], 'llms.txt', 'llms_txt')
+    extracted_llms_full_txt = check_llm_files(state['input_url'], 'llms_full.txt', 'llms_full_txt')
